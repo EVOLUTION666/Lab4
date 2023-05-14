b73425b9289984233ae3cefbacadb5c7db3a527d
Andrey <49302070+EVOLUTION666@users.noreply.github.com>
2023-05-14 16:54:08 +0300

    Update regexp.py

--- regexp.py
+++ regexp.py
@@ -1,7 +1,7 @@
 import re
 
 txt = ""
-with open("text.txt", "r", encoding='utf-8') as f:
+with open("text_ep.txt", "r", encoding='utf-8') as f:
     # noinspection PyRedeclaration
     txt = f.readlines()
     # print(txt)
@@ -25,57 +25,53 @@ def error_symbols(arg):
     arg = re.sub(r" ", "", arg)
     return arg
 
+with open("text.txt", mode="w", encoding="utf-8") as file:
+    for line in txt:
+        line = line.strip()
+        print(line)
+        line = line.split("|")
+        has_empty_elements = False
+        for elem in line:
+            if not elem:
+                has_empty_elements = True
+                break
 
-for line in txt:
-    line = line.strip()
-    print(line)
-    line = line.split(":")
-    has_empty_elements = False
-    for elem in line:
-        if not elem:
-            has_empty_elements = True
-            break
+        if len(line) != 4 or has_empty_elements:
+            continue
 
-    if len(line) != 4 or has_empty_elements:
-        continue
+        name, age, tel, mail = line
 
-    name, age, tel, mail = line
+        name = error_symbols(name)
+        age = error_symbols(age)
+        tel = error_symbols(tel)
+        mail = error_symbols(mail)
 
-    name = error_symbols(name)
-    age = error_symbols(age)
-    tel = error_symbols(tel)
-    mail = error_symbols(mail)
+        name = re.search(name_pattern, name)
+        age = re.search(age_pattern, age)
+        tel = re.search(tel_pattern, tel)
+        mail = re.search(mail_pattern, mail)
 
-    name = re.search(name_pattern, name)
-    age = re.search(age_pattern, age)
-    tel = re.search(tel_pattern, tel)
-    mail = re.search(mail_pattern, mail)
+        if name is not None:
+            file.write(name.group(0) + "|")
+        else:
+            file.write("Invalid name" + "|")
+        assert name != " "
 
-    if name is not None:
-        print(name.group(0), end=":")
-    else:
-        print("Invalid name", end=":")
-    assert name != " "
+        if age is not None:
+            file.write(age.group(0) + "|")
+        else:
+            file.write("Invalid age" + "|")
+        assert age != " "
 
-    if age is not None:
-        print(age.group(0), end=":")
-    else:
-        print("Invalid age", end=":")
-    assert age != " "
+        if tel is not None:
+            file.write(tel.group(0) + "|")
+            
+        else:
+            file.write("Invalid tel" + "|")
+        assert tel != " "
 
-    if tel is not None:
-        print(tel.group(0), end=":")
-        
-    else:
-        print("Invalid tel", end=":")
-    assert tel != " "
-
-    if mail is not None:
-        print(mail.group(0))
-    else:
-        print("Invalid mail")
-    print()
-    assert mail != " "
-
-
-    
+        if mail is not None:
+            file.write(mail.group(0) + "\n")
+        else:
+            file.write("Invalid mail" + "\n")
+        assert mail != " "
