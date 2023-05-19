8071817527f0d4a86740a4099fd11d3ea1285741
Andrey <49302070+EVOLUTION666@users.noreply.github.com>
2023-05-19 19:01:59 +0300

    Update pr4_pytest.py

--- pr4_pytest.py
+++ pr4_pytest.py
@@ -1,19 +1,7 @@
-import regexp
+from regexp import regex_matcher
 
-def test1_get_digits():
-    assert regexp.get_digits('555') == '555'
-
-def test2_get_digits():
-    assert regexp.get_digits('asa555') == '555'
-
-def test1_change_num():
-    assert regexp.change_num('+79000000000') == '+1(900)000-00-00'
-    assert regexp.change_num('79000000000') == '+1(900)000-00-00'
-    assert regexp.change_num('+7(900) 000-00-00') == '+1(900)000-00-00'
-    assert regexp.change_num('+900\n0000000') == '+1(900)000-00-00'
-    assert regexp.change_num('+7(900)) 000-00-00') == '+1(900)000-00-00'
-
-def test2_change_num():
-    assert regexp.change_num('+900 аыфаы\n0000000') == '+900 аыфаы\n0000000'
-    assert regexp.change_num('+7900 000 00фф 00') == '+7900 000 00фф 00'
-    assert regexp.change_num('-7900000:0000') == '-7900000:0000'
+def test1_regex_matcher():
+    with open("text_ep.txt", mode="r", encoding="utf-8") as file:
+        lines = file.readlines()
+        new_text = regex_matcher(lines)
+    assert new_text == 'ИванИванов|27|+79990001111|Invalid mail\nИванИванов|27|+79990001111|example@yandex.ru\n'
